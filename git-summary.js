f5c013154a5bf5058abb8e7d49dd312f4004b6f7
iamdevlinph <iamdevlinph@gmail.com>
2018-07-24 17:52:48 +0800

    Fix #1

--- git-summary.js
+++ git-summary.js
@@ -1,13 +1,20 @@
 #!/usr/bin/env node
 
 const args = process.argv.slice(2);
-const options = args[0];
-const path = args[1];
-const currentPath = process.cwd(); // if no path is passed, will default to the working directory where command was executed
+const options = args[0]; // options
+const dir = args[1]; // dir to check
+const workingDirectory = process.cwd(); // if no dir is passed, will default to the working directory where command was executed
+
+const path = require('path') // npm install path
+const installationDir = path.dirname(require.resolve('git-summary/package.json')) // installation path of the module
+
 const shell = require('shelljs');
 
-const run = (options = '',  path = currentPath) => {
-  shell.exec(`./git-summary.sh ${options} ${path}`);
+const run = (options = '',  path = workingDirectory) => {
+  if (!options) { // if there is no options. set options to be same as path
+    options = path;
+  }
+  shell.exec(`${installationDir}/git-summary.sh ${options} ${path}`);
 }
 
-run(options, path);
+run(options, dir);
