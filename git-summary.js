f5b60a1746154a85193a0b58eb8b6295c693c65a
iamdevlinph <iamdevlinph@gmail.com>
2018-07-24 17:08:43 +0800

    Create as an npm package

---
+++ git-summary.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const args = process.argv.slice(2);
+const options = args[0];
+const path = args[1];
+const currentPath = process.cwd(); // if no path is passed, will default to the working directory where command was executed
+const shell = require('shelljs');
+
+const run = (options,  path = currentPath) => {
+  shell.exec(`./git-summary.sh ${options} ${path}`);
+}
+
+run(options, path);
